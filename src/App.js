1e2100c9630d3250abb1cee86c1ed428d0cf1b5e
rushikeshgadkar <gadkarrushikesh906@gmail.com>
2023-08-24 10:42:42 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import Product from "./Product";
+import SingleProduct from './SingleProduct';
+import Cart from './Cart';
+import ErrorPage from "./ErrorPage";
+import Header from "./component/Header";
+import './App.css'
+// import {GlobalStyle} from './GlobalStyle';
+// import { ThemeProvider } from "styled-components";
+const App = () => {
 
-function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    // <ThemeProvider theme={theme}>
+       <Router >
+        <Header />
+        {/* <GlobalStyle /> */}
+         <Routes >
+            <Route  path="/" element={<Home />} />
+            <Route  path="/about" element={<About />} />
+            <Route  path="/products" element={<Product />} />
+            <Route  path="/contact" element={<Contact />} />
+            <Route  path="/singleproduct/:id" element={<SingleProduct />} />
+            <Route  path="/cart" element={<Cart />} />
+            <Route  path="*" element={<ErrorPage />} />
+         
+         </Routes>
+       </Router>
+      //  </ThemeProvider>
+    
   );
-}
+};
 
 export default App;
